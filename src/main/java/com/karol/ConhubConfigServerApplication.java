1f340dd399f3bf30aa9573ca8e57aa7ec5fd69ce
KarolWojtas <karol.wojtas.aim@gmail.com>
2018-08-26 17:26:52 +0200

    linked eureka and this

--- src/main/java/com/karol/ConhubConfigServerApplication.java
+++ src/main/java/com/karol/ConhubConfigServerApplication.java
@@ -2,8 +2,12 @@ package com.karol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConhubConfigServerApplication {
 
 	public static void main(String[] args) {
